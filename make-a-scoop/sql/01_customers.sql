7d1daf461d8626dbf843dc2a4af52f0f7aa27655
lattawanksp <lattawan.ksp@gmail.com>
2026-03-12 15:51:02 +0700

    added new customers

--- make-a-scoop/sql/01_customers.sql
+++ make-a-scoop/sql/01_customers.sql
@@ -1,4 +1,6 @@
 TRUNCATE TABLE Customers RESTART IDENTITY CASCADE;
 INSERT INTO Customers (customer_id, name, email, phone, address) VALUES
 (1, 'Naruto', 'Narunaru@gmail.com', '0123456789', 'Konoha'),
-(2, 'Sanji', 'Sanjiblackleg@gmail.com', '0789456123', 'North Blue');
+(2, 'Sanji', 'Sanjiblackleg@gmail.com', '0789456123', 'North Blue'),
+(3, 'Gojou', 'Gojo.sensei@gmail.com', '0102030405', 'Jujutsu'),
+(4, 'Nagi', 'NagiSeishirooooo@gmail.com', '0908070405', 'Blue Lock');
