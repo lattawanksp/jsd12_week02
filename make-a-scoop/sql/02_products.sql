66681fee842724a8e82b6244581950d0a7209eb6
lattawanksp <lattawan.ksp@gmail.com>
2026-03-12 16:50:34 +0700

    added-stock-in-products

--- make-a-scoop/sql/02_products.sql
+++ make-a-scoop/sql/02_products.sql
@@ -1,5 +1,5 @@
 TRUNCATE TABLE Products RESTART IDENTITY CASCADE;
-INSERT INTO Products (product_id, product_name, flavor, description, price) VALUES
-(1, 'Royal Vanilla Dream', 'Vanilla', 'Smooth vanilla ice cream made with premium vanilla beans from Madagascar.', 70.00),
-(2, 'Royal Belgian Chocolate', 'Chocolate', 'Creamy chocolate ice cream made with famous cocoa from Belgium.', 85.00),
-(3, 'Strawberry Pop', 'Strawberry', 'Creamy strawberry ice cream made with fresh strawberries from Japan.', 80.00);
+INSERT INTO Products (product_id, product_name, flavor, description, stock, price) VALUES
+(1, 'Royal Vanilla Dream', 'Vanilla', 'Smooth vanilla ice cream made with premium vanilla beans from Madagascar.', 50, 70.00),
+(2, 'Royal Belgian Chocolate', 'Chocolate', 'Creamy chocolate ice cream made with famous cocoa from Belgium.', 30, 85.00),
+(3, 'Strawberry Pop', 'Strawberry', 'Creamy strawberry ice cream made with fresh strawberries from Japan.', 45, 80.00);
